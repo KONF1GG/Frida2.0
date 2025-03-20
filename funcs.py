9cd0b25ba134f3adb01ca19409aefb83be3c5c85
Leonty <krokxa228@gmail.com>
2025-03-20 19:07:02 +0500

    change model on path

--- funcs.py
+++ funcs.py
@@ -4,9 +4,10 @@ from transformers import AutoTokenizer, AutoModel
 import hashlib
 from torch import Tensor
 
+model_path = "/root/.cache/huggingface/hub/models--intfloat--multilingual-e5-large"
 
-model = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
-tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
+model = AutoModel.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
